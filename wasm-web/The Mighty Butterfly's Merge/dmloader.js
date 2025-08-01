f49223dffca455efbd47b78b932b30600cede366
pgs <pablosonaglioni@gmail.com>
2025-08-01 01:51:53 -0300

    mbm versión para mostrar nuevas mariposas de liberación.

--- wasm-web/The Mighty Butterfly's Merge/dmloader.js
+++ wasm-web/The Mighty Butterfly's Merge/dmloader.js
@@ -72,6 +72,8 @@ var CUSTOM_PARAMETERS = {
         var prevInnerHeight = -1;
         
         
+        buttonHeight = 42;
+        
         // Hack for iOS when exit from Fullscreen mode
         if (is_iOS) {
             window.scrollTo(0, 0);
